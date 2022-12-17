ff08cc6b92363973abc921829ff80d5c7dd7a27a
PavelBV <alkatrazzzzz@mail.ru>
2022-12-18 01:48:59 +0300

    added -ewk

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,18 +10,20 @@ class Main {
 
     companion object {
         private const val ALGORITHM = "AES"
+
         @JvmStatic
         fun main(args: Array<String>) {
-            if (args.size == 1) {
-                if (args[0] == "-h" || args[0] == "help") {
-                    println("-e : encrypt file with path (-e (Path))")
-                    println("-d : crypt file with key and path (-d (String) (String))")
-                    exitProcess(0)
-                }
-            }
             if (args.isEmpty()) {
                 println("Input arguments")
                 exitProcess(-1)
+            }
+            if (args.size == 1) {
+                if (args[0] == "-h" || args[0] == "help") {
+                    println("-e : encrypt file with path (-e (path))")
+                    println("-d : decrypt file with key and path (-d (key) (path))")
+                    println("-ewc : encrypt file with key and path (-ewk (key) (path))")
+                    exitProcess(0)
+                }
             }
             when (args[0]) {
                 "-e" -> {
@@ -38,6 +40,13 @@ class Main {
                     } else decrypt(args[1], args[2])
                 }
 
+                "-ewk" -> {
+                    if (args.size < 3) {
+                        println("Input mode, key and file path")
+                        exitProcess(-1)
+                    } else encryptWithKey(args[1], args[2])
+                }
+
                 else -> {
                     println("Wrong arguments")
                     exitProcess(-1)
@@ -46,10 +55,41 @@ class Main {
         }
 
         @JvmStatic
+        private fun encryptWithKey(key: String, path: String) {
+            val file = File(path)
+            if (file.exists() && file.isFile) {
+                try {
+                    val inputData = file.readBytes()
+                    val keySpec = SecretKeySpec(Base64.getDecoder().decode(key), ALGORITHM)
+                    val cipher = Cipher.getInstance(ALGORITHM).also { it.init(Cipher.ENCRYPT_MODE, keySpec) }
+                    val encrypted = cipher.doFinal(inputData)
+                    val newFile = File(file.parent + File.separator + "encrypted")
+                    newFile.apply {
+                        if (exists() && isFile) {
+                            delete()
+                        }
+                    }
+                    newFile.apply {
+                        createNewFile()
+                        setWritable(true)
+                        writeBytes(encrypted)
+                    }
+                    println("Encryption successful")
+                    exitProcess(0)
+                } catch (exc: Exception) {
+                    exc.printStackTrace()
+                    exitProcess(-1)
+                }
+            } else {
+                println("File not found")
+                exitProcess(-1)
+            }
+        }
+
+        @JvmStatic
         fun encrypt(path: String) {
-            val secureRandom = SecureRandom()
             val keygen = KeyGenerator.getInstance(ALGORITHM).also {
-                it.init(256, secureRandom)
+                it.init(256, SecureRandom())
             }
             val sks = SecretKeySpec(keygen.generateKey().encoded, ALGORITHM)
             val key = String(Base64.getEncoder().encode(sks.encoded))
@@ -58,11 +98,13 @@ class Main {
             if (file.exists() && file.isFile) {
                 try {
                     val inputData = file.readBytes()
-                    val cipher = Cipher.getInstance(ALGORITHM)
-                    val keySpec = SecretKeySpec(Base64.getDecoder().decode(key), ALGORITHM)
-                    cipher.init(Cipher.ENCRYPT_MODE, keySpec)
+                    val cipher = Cipher.getInstance(ALGORITHM).also { it.init(Cipher.ENCRYPT_MODE, sks) }
+//                    val keySpec = SecretKeySpec(Base64.getDecoder().decode(key), ALGORITHM)
                     val encrypted = cipher.doFinal(inputData)
                     val newFile = File(file.parent + File.separator + "encrypted")
+                    if (newFile.exists() && newFile.isFile) {
+                        newFile.delete()
+                    }
                     newFile.apply {
                         createNewFile()
                         setWritable(true)
@@ -85,16 +127,18 @@ class Main {
             try {
                 val file = File(path)
                 val target = File(file.parent + File.separator + "decrypted")
+                if (target.exists() && target.isFile) {
+                    target.delete()
+                }
                 target.apply {
                     createNewFile()
                     setWritable(true)
                 }
                 val inputData = file.readBytes()
-                val cipher = Cipher.getInstance(ALGORITHM)
                 val keySpec = SecretKeySpec(Base64.getDecoder().decode(key), ALGORITHM)
-                cipher.init(Cipher.DECRYPT_MODE, keySpec)
-                val encrypted = cipher.doFinal(inputData)
-                target.writeBytes(encrypted)
+                val cipher = Cipher.getInstance(ALGORITHM).also { it.init(Cipher.DECRYPT_MODE, keySpec) }
+                val decrypted = cipher.doFinal(inputData)
+                target.writeBytes(decrypted)
                 println("Decryption successful")
                 exitProcess(0)
             } catch (exc: Exception) {
